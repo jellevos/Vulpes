2af2e65a67d8c0514db4e8f1b2f137f96a10e678
jelle <j.v.jelle@gmail.com>
2019-05-18 17:25:47 +0200

    Setup travis with a simple linter

--- src/definition/robot.py
+++ src/definition/robot.py
@@ -1,11 +1,17 @@
+"""
+The robot module contains the classes that define a robot.
+"""
 
 
-class Robot(object):
+class Robot:
+    """
+    The Robot class represents the physical entity of the robot in the software.
+    """
 
     def __init__(self, physical_definition_file: str):
         """
         Initializes a Robot object from the physical definition represented by an SDF or URDF file.
 
-        :param physical_definition: SDF or URDF file representing the physical robot
+        :param physical_definition_file: SDF or URDF file representing the physical robot
         """
         self._physical_definition_file = physical_definition_file
